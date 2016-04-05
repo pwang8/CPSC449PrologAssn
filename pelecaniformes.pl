ad9e8eef7e6c67ec67491af07c90eabacd9c83a5
Paul Wang <p.wang8@outlook.com>
2016-04-04 18:02:55 -0600

    added first clause hasParent

---
+++ pelecaniformes.pl
@@ -0,0 +1,135 @@
+order(pelecaniformes).
+
+family(pelecanidae).
+family(ardeidae).
+family(threskiornithdae).
+
+genus(pelecanus).
+genus(botaurus).
+genus(ixobrychus).
+genus(ardea).
+genus(egretta).
+genus(bubulcus).
+genus(butorides).
+genus(nycticorax).
+genus(nyctanassa).
+genus(eudocimus).
+genus(plegadis).
+genus(platalea).
+
+species(erythrorhynchos).
+species(occidentalis).
+species(lentiginosus).
+species(exilis).
+species(herodias).
+species(alba).
+species(thula).
+species(caerulea).
+species(tricolor).
+species(rufescens).
+species(ibis).
+species(virescens).
+species(nycticorax).
+species(violacea).
+species(albus).
+species(falcinellus).
+species(chihi).
+species(ajaja).
+
+%hasParent(species,genus)
+hasParent(erythrorhynchos, pelecanus).
+hasParent(occidentalis, pelecanus).
+hasParent(lentiginosus, botaurus).
+hasParent(exilis, ixobrychus).
+hasParent(herodias, ardea).
+hasParent(alba, ardea).
+hasParent(thula, egretta).
+hasParent(caerulea, egretta).
+hasParent(tricolor, egretta).
+hasParent(rufescens, egretta).
+hasParent(ibis, bubulcus).
+hasParent(virescens, butorides).
+hasParent(nycticorax, nycticorax).
+hasParent(violacea, nyctanassa).
+hasParent(albus, eudocimus).
+hasParent(falcinellus, plegadis).
+hasParent(chihi, plegadis).
+hasParent(ajaja, platalea).
+%hasParent(genus, family) ----------------------------
+hasParent(pelecanus, pelecanidae).
+hasParent(botaurus, ardeidae).
+hasParent(ixobrychus, ardeidae).
+hasParent(ardea, ardeidae).
+hasParent(egretta, ardeidae).
+hasParent(bubulcus, ardeidae).
+hasParent(butorides, ardeidae).
+hasParent(nycticorax, ardeidae).
+hasParent(nyctanassa, ardeidae).
+hasParent(eudocimus, threskiornithdae).
+hasParent(plegadis, threskiornithdae).
+hasParent(platalea, threskiornithdae).
+%hasParent(family, order) ----------------------------
+hasParent(pelecanidae, pelecaniformes).
+hasParent(ardeidae, pelecaniformes).
+hasParent(threskiornithdae, pelecaniformes).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
